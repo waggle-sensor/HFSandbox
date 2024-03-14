90efcbc56f462b21c396cb09b17bfcb0b267768f
Dario Dematties <ddematties@anl.gov>
2024-03-14 18:48:47 +0000

    First experiment

--- run_vip-llava-13b-hf_model.py
+++ run_vip-llava-13b-hf_model.py
@@ -4,6 +4,7 @@ import os
 import csv
 import shutil
 import requests
+from random import shuffle
 from glob import glob
 from PIL import Image
 import time
@@ -26,10 +27,12 @@ def main():
 
     directory='/images'
     files = [y for x in os.walk(directory) for y in glob(os.path.join(x[0], '*.jpg'))]
+    shuffle(files)
 
     os.makedirs('/RESULTS/FIRE_IMAGES/', mode=0o777, exist_ok=True)
     RESULTS=[]
     BAG_OF_WORDS=[]
+    NEUTRAL_BAG_OF_WORDS=[]
     #directory='/images'
     counter=0
     #for FILE in os.listdir(directory):
@@ -41,43 +44,83 @@ def main():
 
         image_file=FILE
         #image_file=os.path.join(directory, FILE)
-        response=run_llava(model, processor, user_question, user_prompt, image_file)
+        response, time=run_llava(model, processor, user_question, user_prompt, image_file)
         print(FILE)
         #print(response)
         pattern='###Assistant:'
         print(response[-1:])
-        RESULTS.append(FILE + ', ' + response[-1:])
+        RESULTS.append(FILE + ', ' + response[-1:] + ', ' + str(time))
         if int(response[-1])==1: # FIRE!!!
-            if not os.path.isfile(image_file):
+            #if not os.path.isfile(image_file):
+            try:
+                #print('Trying to copy '+image_file+' into '+'/RESULTS/FIRE_IMAGES/')
                 shutil.copy(image_file, '/RESULTS/FIRE_IMAGES/')
+            except IOError:
+                print("Unable to copy file: ", image_file)
 
             user_question="An expert inspected the image and claimed to see a wildfire in it."
             #user_prompt="A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions."
             #user_prompt='Please generate the number 1 if there is fire in the image, otherwise generate the number 0. Only one number has to be generated in your response.'
             user_prompt='Please, generate a detailed explanation of why the expert thinks this way.'
-            response=run_llava(model, processor, user_question, user_prompt, image_file).split(pattern, 1)[1]
+            response, time=run_llava(model, processor, user_question, user_prompt, image_file)
+            response=response.split(pattern, 1)[1]
             BAG_OF_WORDS.append(image_file)
             BAG_OF_WORDS.append(response)
+            BAG_OF_WORDS.append(str(time))
             print('--------------->>>>>>>>>>>>>>>>>>')
             print(response)
             #break
             
+            user_question="As a WildfireWatcher, your task is to scrutinize optical camera images for wildfire indicators."
+            #user_prompt="A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions."
+            #user_prompt='Please generate the number 1 if there is fire in the image, otherwise generate the number 0. Only one number has to be generated in your response.'
+            #user_prompt='Please, generate a detailed explanation of why the expert thinks this way.'
+            user_prompt='Issue a one-word alert based on your findings: WILDFIRE if smoke plumes, dispersed smoke, or visible fire are detected; NA for absence of fire or smoke.'
+            response, time=run_llava(model, processor, user_question, user_prompt, image_file)
+            response=response.split(pattern, 1)[1]
+            BAG_OF_WORDS.append(response)
+            BAG_OF_WORDS.append(str(time))
+            print('--------------->>>>>>>>>>>>>>>>>>')
+            print(response)
+            #break
 
-        counter += 1
-        if counter > 1000:
-            break
+            user_question="Is there fire in the image?"
+            #user_prompt="A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions."
+            #user_prompt='Please generate the number 1 if there is fire in the image, otherwise generate the number 0. Only one number has to be generated in your response.'
+            #user_prompt='Please, generate a detailed explanation of why the expert thinks this way.'
+            user_prompt='Please, provide a detailled explanation of what you see in the image.'
+            response, time=run_llava(model, processor, user_question, user_prompt, image_file)
+            response=response.split(pattern, 1)[1]
+            NEUTRAL_BAG_OF_WORDS.append(image_file)
+            NEUTRAL_BAG_OF_WORDS.append(response)
+            NEUTRAL_BAG_OF_WORDS.append(str(time))
+            print('--------------->>>>>>>>>>>>>>>>>>')
+            print(response)
+            #break
+
+
+        #counter += 1
+        #if counter > 50:
+        #    break
 
     os.chmod("/RESULTS/FIRE_IMAGES/", 0o777)
     with open('/RESULTS/output.csv','w') as result_file:
         wr = csv.writer(result_file, dialect='excel')
         wr.writerow(RESULTS)
       
+    os.chmod("/RESULTS/output.csv", 0o666)
 
     with open('/RESULTS/bag_of_words.csv','w') as bow_file:
         wr = csv.writer(bow_file, dialect='excel')
         wr.writerow(BAG_OF_WORDS)
 
+    os.chmod("/RESULTS/bag_of_words.csv", 0o666)
 
+    with open('/RESULTS/neutral_bag_of_words.csv','w') as bow_file:
+        wr = csv.writer(bow_file, dialect='excel')
+        wr.writerow(NEUTRAL_BAG_OF_WORDS)
+
+    os.chmod("/RESULTS/neutral_bag_of_words.csv", 0o666)
 
 
 
@@ -99,7 +142,7 @@ def run_llava(model, processor,
     end_t = time.time()
     #print(processor.decode(output[0][2:], skip_special_tokens=True))
     #print('Time elapsed: ', end_t-start_t) 
-    return processor.decode(output[0][2:], skip_special_tokens=True)
+    return processor.decode(output[0][2:], skip_special_tokens=True), end_t-start_t 
 
 
 
